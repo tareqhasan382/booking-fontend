bc9a4b95a36dba70bf6bc6e9b8756c1bc318a3f0
tareqhasan382 <tareqhasan382@gmail.com>
2023-11-03 15:58:08 +0600

    redux query

--- src/components/package/DetailsPage.tsx
+++ src/components/package/DetailsPage.tsx
@@ -1,12 +1,4 @@
-"use client";
-
-import { useGetSinglePackageQuery } from "@/redux/api/packageApi";
-
-const DetailsPage = ({ params }: { params: { slug: string } }) => {
-  console.log(params);
-  const { data, isLoading } = useGetSinglePackageQuery({ params });
-  const packages = data?.packages?.data;
-  console.log(packages);
+const DetailsPage = () => {
   return <div></div>;
 };
 
