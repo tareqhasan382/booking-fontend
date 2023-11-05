668353a8e442012949588e0773ad3ff7b4588d9f
tareqhasan382 <tareqhasan382@gmail.com>
2023-11-06 01:35:17 +0600

    fevorites

--- src/app/(user-layout)/package/page.tsx
+++ src/app/(user-layout)/package/page.tsx
@@ -1,14 +1,20 @@
-"use client";
-import { useGetPackagesQuery } from "@/redux/api/packageApi";
+import { getBaseUrl } from "@/helpers/config/envConfig";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 //?limit=8
-const page = () => {
-  let query: {};
-
-  const { data, isLoading } = useGetPackagesQuery({ undefined });
-
+const page = async () => {
+  const res = await fetch(`${getBaseUrl()}/api/v1/trips`, {
+    cache: "no-store",
+  });
+  const { data } = await res.json();
+  // console.log(data);
+  // const { data: tips, isLoading } = useGetPackagesQuery({ undefined });
+  //console.log("data:", tips);
+  //const data = tips?.data;
+  // const meta = tips?.packages.meta;
+  // console.log("tips:", data);
+  // console.log("meta:", meta);
   return (
     <div className=" lg:px-48 md:px-20 h-auto  w-full ">
       <div className=" flex flex-col justify-center items-center pt-10 text-black px-7 ">
